187be99059fb83e70d5593619e92e8e0c8c4d21f
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-29 13:56:24 -0700

    Improve user input choices

--- wordcount.py
+++ wordcount.py
@@ -20,23 +20,27 @@ def write_to_file(big_data, output_file_name = "output_stat.txt"):
             output.write("%s %s\n" % (key, value))
 
 
-data_choice = raw_input("Print result on screen or in file (S/F):")
+while True:
+    data_choice = raw_input("Print result on screen or in file (S/F):")
 
-file_name = raw_input("Type the file_name to work on\
-    or Enter for default choice: ")
+    file_name = raw_input("Type the file_name to work on or Enter for default choice: ")
 
-if data_choice == "S":
-    counted_words = word_count("test.txt")
-    for key, value in counted_words.items():
-        print key, value
+    if data_choice == "S":
+        counted_words = word_count("test.txt")
+        for key, value in counted_words.items():
+            print key, value
+        break
 
-else:
-    if file_name == "":
-        file_name = "twain.txt"
+    elif data_choice == "F":
+        if file_name == "":
+            file_name = "twain.txt"
 
-    big_counted_words = word_count(file_name)
-    write_to_file(big_counted_words)
-    print "Check out output in file"
+        big_counted_words = word_count(file_name)
+        write_to_file(big_counted_words)
+        print "Check out output in file"
+        break
+    else:
+        continue
 
 # cat file_name - prints out file on screen
 # touch file_name - creates new file
